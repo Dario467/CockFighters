df1d4d342705377f848fdf54975a8f410da1b992
Dario467 <funky467@gmail.com>
2025-11-16 17:13:37 -0600

    reponsive UI addition

--- cock.js
+++ cock.js
@@ -1,9 +1,11 @@
 class Cock {
-    constructor(nombre, vida, poder, sprites) {
+    constructor(nombre, vida, poder,bulletsMax, sprites) {
         this.nombre = nombre;
         this.vida = vida;
         this.vidaMax = vida;
         this.poder = poder;
+        this.bulletsMax = bulletsMax;
+        this.bullets = 0;
         this.sprites = sprites;
     }
 }
--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-const canvas = document.querySelector("canvas");
+const canvas = document.getElementById("canvas1");
 const c = canvas.getContext("2d");
+const canvas2 = document.getElementById("canvas2");
+const c2 = canvas2.getContext("2d");
 
 canvas.width = 1024;
 canvas.height = 576;
+canvas2.width = 1024;
+canvas2.height = 576;
 let awake = false;
 let in_animation = false;
 
-c.fillRect(0,0,canvas.width, canvas.height);
+//c.fillRect(0,0,canvas.width, canvas.height);
+c2.fillRect(0,0,canvas2.width, canvas2.height);
 
 const player1 = new Player({
     x:100,
     y:0+canvas.height-240
 },
 {
-    x:340,
-    y:240
-},
-[
-  new Cock("GunCock",20,8,["./Assets/gallo_sprite_front1.png","./Assets/gallo_sprite_front.png"])
-],
-true);
-
-const player2 = new Player({
     x:canvas.width-480,
     y:60
 },
@@ -30,9 +26,26 @@ const player2 = new Player({
     y:240
 },
 [
-  new Cock("GunCock",20,8,["./Assets/gallo_sprite_front1.png","./Assets/gallo_sprite_front.png"])
+  new Cock("GunCock",15,12,6,["./Assets/gallo_sprite_front1.png","./Assets/gallo_sprite_front.png"])
 ],
-false);
+1);
+
+const player2 = new Player({
+    x:canvas.width-480,
+    y:60
+},
+{
+    x:100,
+    y:0+canvas.height-240
+},
+{
+    x:340,
+    y:240
+},
+[
+  new Cock("GunCock",20,8,6,["./Assets/gallo_sprite_front1.png","./Assets/gallo_sprite_front.png"])
+],
+2);
 
 function gameLoop() {
   if (!awake){
@@ -44,7 +57,14 @@ function gameLoop() {
   window.requestAnimationFrame(gameLoop);
   c.fillStyle = "lightgray";
   c.fillRect(0,0,canvas.width, canvas.height);
-  player1.draw(c,0);
-  player2.draw(c,1);
+  
+  c2.fillStyle = "lightgray";
+  c2.fillRect(0,0,canvas2.width, canvas2.height);
+  
+  player1.draw(c,0,true);
+  player2.draw(c,1,true);
+
+  player1.draw(c2,1,false);
+  player2.draw(c2,0,false);
 }
 gameLoop();

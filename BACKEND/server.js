07fe2418993ea7d322c2d07f62c3392892cb4cfa
juanpabloglez25 <juan.gonzalezm@iteso.mx>
2025-11-24 09:34:56 -0600

    esta vivo el proyecto

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 //crear app
 const app = express();
